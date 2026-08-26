3b4af41e0393d8c6af74026618e67c76e60a185f
Maxime FALCHERO <maximefalchero@gmail.com>
2026-08-26 21:32:44 +0200

    fix: normalize empty customer address fields

--- src/Form/CustomerAddressType.php
+++ src/Form/CustomerAddressType.php
@@ -18,18 +18,21 @@ class CustomerAddressType extends AbstractType
         $builder
             ->add('firstName', TextType::class, [
                 'label' => 'Prénom',
+                'empty_data' => '',
                 'attr' => [
                     'autocomplete' => 'given-name',
                 ],
             ])
             ->add('lastName', TextType::class, [
                 'label' => 'Nom',
+                'empty_data' => '',
                 'attr' => [
                     'autocomplete' => 'family-name',
                 ],
             ])
             ->add('line1', TextType::class, [
                 'label' => 'Adresse',
+                'empty_data' => '',
                 'attr' => [
                     'autocomplete' => 'address-line1',
                 ],
@@ -37,24 +40,28 @@ class CustomerAddressType extends AbstractType
             ->add('line2', TextType::class, [
                 'label' => 'Complément d’adresse',
                 'required' => false,
+                'empty_data' => '',
                 'attr' => [
                     'autocomplete' => 'address-line2',
                 ],
             ])
             ->add('postalCode', TextType::class, [
                 'label' => 'Code postal',
+                'empty_data' => '',
                 'attr' => [
                     'autocomplete' => 'postal-code',
                 ],
             ])
             ->add('city', TextType::class, [
                 'label' => 'Ville',
+                'empty_data' => '',
                 'attr' => [
                     'autocomplete' => 'address-level2',
                 ],
             ])
             ->add('countryCode', CountryType::class, [
                 'label' => 'Pays',
+                'empty_data' => '',
                 'preferred_choices' => [
                     'FR',
                     'BE',
